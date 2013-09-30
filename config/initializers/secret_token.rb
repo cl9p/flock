33687efba27df1ca2e9d783fa582ac932dbc4480
Lee Faus <leefaus@me.com>
2013-09-30 14:52:24 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flock::Application.config.secret_token = '4d42acf3e9cf129c81025b5f4c5dafd128b72aed504444810d7ecf74166d68ce02272ca142f73454faf84a74ffad866e86d8cffc91e03ed5eab29ed6217539bf'
